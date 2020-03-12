babd6e503244b5dcc17c4b413b565bbe2d3adc1b
poezedoez <raggerjonkers@gmail.com>
2020-03-12 14:28:04 +0100

    add relations

--- DistantlySupervisedDataset.py
+++ DistantlySupervisedDataset.py
@@ -17,7 +17,7 @@ def _read_ontology_entities(path):
     ontology_entities = defaultdict(list)
     with open(path, 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader, None) # skip headers
+        next(csv_reader, None)  # skip headers
         for _, class_, instance in csv_reader:
             ontology_entities[class_].append(instance)
 
@@ -28,7 +28,7 @@ def _read_ontology_relations(path):
     ontology_relations = {}
     with open(path, 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
-        next(csv_reader, None) # skip headers
+        next(csv_reader, None)  # skip headers
         for _, head, _, _ in csv_reader:
             ontology_relations[head] = {}
         for _, head, relation, tail in csv_reader:
@@ -44,12 +44,12 @@ def _fuse_subtokens(subtokens, includes_special_tokens=True):
     fused2tok = []
     for i, token in enumerate(tokens):
         if token.startswith('##'):
-            fused_tokens[len(fused_tokens)-1] = fused_tokens[len(fused_tokens)-1] + token.replace('##', '')
+            fused_tokens[len(fused_tokens) - 1] = fused_tokens[len(fused_tokens) - 1] + token.replace('##', '')
         else:
             fused2tok.append(i)
             fused_tokens.append(token)
 
-        tok2fused.append(len(fused_tokens)-1)
+        tok2fused.append(len(fused_tokens) - 1)
 
     return fused_tokens, tok2fused, fused2tok
 
@@ -77,13 +77,13 @@ class DistantlySupervisedDataset:
     """
 
     def __init__(
-            self, 
-            ontology_entities_path="data/ontology_entities.csv", 
+            self,
+            ontology_entities_path="data/ontology_entities.csv",
             ontology_relations_path="data/ontology_relations.csv",
-            document_path="data/ScientificDocuments/", 
-            entity_embedding_path="data/entity_embeddings.json", 
+            document_path="data/ScientificDocuments/",
+            entity_embedding_path="data/entity_embeddings.json",
             output_path="data/DistantlySupervisedDatasets/"
-            ):
+    ):
 
         self.ontology_entities = _read_ontology_entities(ontology_entities_path)
         self.ontology_relations = _read_ontology_relations(ontology_relations_path)
@@ -91,8 +91,8 @@ class DistantlySupervisedDataset:
         self.timestamp = time.strftime("%Y%m%d-%H%M%S")
         self.document_path = document_path
         self.entity_embedding_path = entity_embedding_path
-        self.output_path = output_path+'{}/'.format(self.timestamp)
-        self.statistics = {"classes": Counter(), "tokens": {class_:Counter() for class_ in self.ontology_entities},
+        self.output_path = output_path + '{}/'.format(self.timestamp)
+        self.statistics = {"classes": Counter(), "tokens": {class_: Counter() for class_ in self.ontology_entities},
                            "sentences_useful": 0, "sentences_processed": 0, "entities_total": 0, "tokens_total": 0}
         self.class_arrays = {}
         self.flist = []
@@ -105,7 +105,7 @@ class DistantlySupervisedDataset:
         for sentence_subtokens, document_embeddings, offset in self._iter_sentences(selection):
             self._label_sentence(sentence_subtokens, document_embeddings, offset, knn_labeling)
         end_time = time.time()
-        self.statistics["time_taken"] = int(end_time-start_time)
+        self.statistics["time_taken"] = int(end_time - start_time)
         self._save()
         if verbose:
             self.print_statistics()
@@ -115,18 +115,19 @@ class DistantlySupervisedDataset:
         Path(directory).mkdir(parents=True, exist_ok=True)
 
         # Save dataset
-        with open(self.output_path+'dataset.json', 'w', encoding='utf-8') as json_file:
+        with open(self.output_path + 'dataset.json', 'w', encoding='utf-8') as json_file:
             json.dump(self.dataset, json_file)
 
         # Save statistics
-        with open(self.output_path+'statistics.json', 'w', encoding='utf-8') as json_file:
-            json.dump(self.statistics, json_file) 
+        with open(self.output_path + 'statistics.json', 'w', encoding='utf-8') as json_file:
+            json.dump(self.statistics, json_file)
 
         # Save ontology used
-        shutil.copyfile(args.ontology_path, self.output_path+'ontology.csv')
+        shutil.copyfile(args.ontology_entities_path, self.output_path + 'ontology_entities.csv')
+        shutil.copyfile(args.ontology_relations_path, self.output_path + 'ontology_relations.csv')
 
         # Save list of documents used for the set
-        with open(self.output_path+'filelist.txt', 'w', encoding='utf-8') as txt_file:
+        with open(self.output_path + 'filelist.txt', 'w', encoding='utf-8') as txt_file:
             for file in self.flist:
                 txt_file.write("{} \n".format(file))
 
@@ -136,13 +137,13 @@ class DistantlySupervisedDataset:
                 stats = json.load(f)
         else:
             stats = self.statistics
-         
+
         print("--- STATISTICS ---")
         print("Processed {} sentences of which {} contained at least one entity".format(
             stats["sentences_processed"], stats["sentences_useful"]
         ))
         print("Time taken: {} seconds".format(stats["time_taken"]))
-        tokens_per_entity = stats["tokens_total"]/stats["entities_total"]
+        tokens_per_entity = stats["tokens_total"] / stats["entities_total"]
         print("Every {} tokens an entity occurs".format(tokens_per_entity))
         print("The following classes were found:")
         for class_, count in stats["classes"].items():
@@ -162,10 +163,10 @@ class DistantlySupervisedDataset:
         path = self.document_path
         self.flist = os.listdir(path) if not selection else os.listdir(path)[selection[0]:selection[1]]
         for folder in self.flist:
-            text_path = glob.glob(path+"{}/representations/".format(folder) + "text_sentences|*.tokens")[0]
+            text_path = glob.glob(path + "{}/representations/".format(folder) + "text_sentences|*.tokens")[0]
             with open(text_path, 'r', encoding='utf-8') as text_json:
                 text = json.load(text_json)
-            embeddings_path = glob.glob(path+"{}/representations/".format(folder) +
+            embeddings_path = glob.glob(path + "{}/representations/".format(folder) +
                                         "text_sentences|*word_embeddings.npy")[0]
             embeddings = np.load(embeddings_path)
 
@@ -176,7 +177,7 @@ class DistantlySupervisedDataset:
         fused_string, _, _ = _fuse_subtokens(tokenized_string, includes_special_tokens=False)
         tokens = [token.lower() for token in tokens]
         string_length = len(fused_string)
-        matches = [(occ, occ+string_length) for occ in KnuthMorrisPratt(tokens, fused_string)]
+        matches = [(occ, occ + string_length) for occ in KnuthMorrisPratt(tokens, fused_string)]
 
         return matches
 
@@ -218,7 +219,7 @@ class DistantlySupervisedDataset:
                     if relation:
                         relations.append({"type": relation, "head": head, "tail": tail})
             return relations
-        
+
         def _label_entities(tokens, tok2fused):
             entities = []
             sentence_embeddings = document_embeddings[offset:offset + len(tokens)]
@@ -227,7 +228,7 @@ class DistantlySupervisedDataset:
                     string_matches = self._string_match(fused_tokens, string_instance)
                     knn_matches = self._knn_match(sentence_embeddings, tok2fused, class_) if (
                             knn_labeling and string_matches) else []
-                    matches = set(string_matches+knn_matches)
+                    matches = set(string_matches + knn_matches)
                     # TODO: temp!!
                     matches = knn_matches
                     for start, end in matches:
@@ -236,8 +237,8 @@ class DistantlySupervisedDataset:
                         print("original sentence:", " ".join(fused_tokens))
                         self.statistics["classes"][class_] += 1
                         self.statistics["tokens"][class_][entity_string] += 1
-                        
-                        entities.append({"type":class_, "start":start, "end":end})
+
+                        entities.append({"type": class_, "start": start, "end": end})
             return entities
 
         fused_tokens, tok2fused, _ = _fuse_subtokens(sentence_subtokens)
@@ -279,7 +280,7 @@ class DistantlySupervisedDataset:
             for class_, count_dict in entity_counter.items():
                 for token, count in count_dict.items():
                     summed_embedding = entity_embeddings[class_][token]
-                    entity_embeddings[class_][token] = summed_embedding / count
+                    entity_embeddings[class_][token] = (summed_embedding / count).tolist()
 
             return entity_embeddings
 
@@ -305,7 +306,7 @@ if __name__ == "__main__":
     parser.add_argument('--ontology_relations_path', type=str, default="data/ontology_relations.csv",
                         help="path to the ontology relations file")
     parser.add_argument('--document_path', type=str, help='path to the folder containing scientific documents',
-                        default="data/ScientificDocuments/all/")
+                        default="data/ScientificDocuments/")
     parser.add_argument('--output_path', type=str, default="data/DistantlySupervisedDatasets/", help="output path")
     parser.add_argument('--entity_embedding_path', type=str, default="data/entity_embeddings.json",
                         help="path to file of precalculated lexical embeddings of the entities")
@@ -314,8 +315,8 @@ if __name__ == "__main__":
     parser.add_argument('--knn_labeling', type=int, default=0,
                         help="use knn unsupervised labeling in conjunction with default string matching")
     args = parser.parse_args()
-    dataset = DistantlySupervisedDataset(args.ontology_path, args.document_path,
-                                         args.output_path, args.entity_embedding_path)
+    dataset = DistantlySupervisedDataset(args.ontology_entities_path, args.ontology_relations_path, args.document_path,
+                                         args.entity_embedding_path, args.output_path)
     # dataset.create(knn_labeling=args.knn_labeling, selection=tuple(args.selection))
-    dataset._calculate_entity_embeddings()
+    dataset._load_class_arrays()
     dataset._save()
