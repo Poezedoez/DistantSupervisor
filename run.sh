26d24f8747b4070b6639444f8a510d5548276a4a
poezedoez <raggerjonkers@gmail.com>
2020-05-23 16:31:31 +0200

    runrun

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 COS_THETA=0.83
 COMBINED_LABELING=2
 V=4
-ENT_PATH="data/ontology/v${V}/ontology_entities.csv" 
-REL_PATH="data/ontology/v${V}/ontology_relations.csv" 
-REDUCE="none"
-EMB_PATH="data/ontology/v${V}/faiss/"
+TOKEN_POOLING="none"
+MENTION_POOLING="none"
 DATE=`date +%d_%m_%Y_%H_%M_%S`
 RUN_NAME="${REDUCE}_filtered_$DATE";
 
@@ -15,7 +13,8 @@ python DistantSupervisor.py  \
     --label_strategy $COMBINED_LABELING \
     --cos_theta $COS_THETA \
     --ontology_version $V \
-    --f_reduce $REDUCE \
+    --token_pooling $TOKEN_POOLING \
+    --mention_pooling $MENTION_POOLING \ 
     --filter_sentences
 
 python DistantSupervisor.py  \
@@ -25,7 +24,8 @@ python DistantSupervisor.py  \
     --label_strategy $COMBINED_LABELING \
     --cos_theta $COS_THETA \
     --ontology_version $V \
-    --f_reduce $REDUCE \
+    --token_pooling $TOKEN_POOLING \
+    --mention_pooling $MENTION_POOLING \ 
     --filter_sentences
 
 # python DistantSupervisor.py  \
@@ -34,8 +34,6 @@ python DistantSupervisor.py  \
 #     --timestamp_given \
 #     --label_strategy $COMBINED_LABELING \
 #     --cos_theta $COS_THETA \
-#     --ontology_entities_path $ENT_PATH \
-#     --ontology_relations_path $REL_PATH \
-#     --entity_embedding_path $EMB_PATH \
-#     --f_reduce $REDUCE \
+#     --token_pooling $TOKEN_POOLING \
+#     --mention_pooling $MENTION_POOLING \ 
 #     --filter_sentences
